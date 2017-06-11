b454723491842766cea312baa991f4ef6117c341
Thomas Zimmermann <bugs@vdm-design.de>
2017-06-11 20:37:02 +0200

    initial version

---
+++ QIpp.h
@@ -0,0 +1,58 @@
+#ifndef QIpp_H
+#define QIpp_H
+
+#include <QObject>
+#include <QHash>
+
+#include <cups/cups.h>
+
+class QTimer;
+class QUrl;
+class QIpp : public QObject
+{
+	Q_OBJECT
+
+public:
+	QIpp();
+	QIpp(const char *host, const char *resource = "/ipp", const char *scheme ="ipp", int port = ippPort(), int interval = 1000);
+	QIpp(const QUrl *url, int interval = 1000);
+	virtual ~QIpp();
+
+public slots:
+	void Print(int fd, const char *jobname = "printjob") const;
+	void Print(const char *filename) const;
+	void getPrinterStatus();
+	inline void setHost(const char *host) {
+		m_host = QString(host);
+	};
+	inline void setScheme(const char *scheme) {
+		m_scheme = QString(scheme);
+	};
+	inline void setResource(const char *resource) {
+		m_resource = QString(resource);
+	};
+	inline void setPort(int port) {
+		m_port = port;
+	};
+	void setStatusInterval(int interval);
+
+signals:
+	void printerStatusChanged(QString state, QString statereason);
+
+private:
+	void init();
+	ipp_t *doRequest(ipp_t* request, int fd = -1, const char *filename = NULL) const;
+	QTimer *m_timer;
+	QString m_state;  // printer state (idle, processing, stopped)
+	QString m_statereason; // reason for state, see {{{m_states}}}
+	int m_interval; // printer status retrival interval
+	QString m_resource; // path part of the ipp URL
+	int m_port; // ipp port, standard 631
+	QString m_host; // printer host
+	QString m_scheme; // ipp URL scheme (ipp, http, ...)
+	// https://tools.ietf.org/html/rfc2911#section-4.4.12
+	QHash<QString,QString> *m_states;
+
+};
+
+#endif // QIpp_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include "QIpp.h"
+
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+    QIpp qipp;
+    return app.exec();
+}
